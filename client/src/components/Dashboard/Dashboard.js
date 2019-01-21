4094972ee1664b8ef50e786e30a10d0a175ede68
MathisZerbib <mathis.zerbib@gmail.com>
2019-01-21 21:44:21 +0100

    COUCOU

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -24,6 +24,7 @@ export class Dashboard extends React.Component {
                 >
                 Se déconnecter
                 </Button>
+                <p>COUCOU</p>
             </div>
         )
     }
